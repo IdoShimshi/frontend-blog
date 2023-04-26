7ba26eb2c27256514f0df4bc634d6d3a04c2b87a
Ido Shimshi <idshimshi@gmail.com>
2023-04-26 18:25:47 +0300

    added pagination, still need to populate db

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,40 +4,100 @@ import Layout from "../components/Layout";
 import Post, { PostProps } from "../components/Post";
 import prisma from '../lib/prisma'
 
-export const getServerSideProps: GetServerSideProps = async () => {
-  const feed = await prisma.post.findMany({
-    where: {
-      published: true,
-    },
-    include: {
-      author: {
-        select: {
-          name: true,
+const PAGE_SIZE = 2;
+
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+  const page = parseInt(query.page as string) || 1;
+  const skip = (page - 1) * PAGE_SIZE;
+
+  const [feed, count] = await Promise.all([
+    prisma.post.findMany({
+      where: {
+        published: true,
+      },
+      include: {
+        author: {
+          select: {
+            name: true,
+          },
         },
       },
-    },
-  });
+      take: PAGE_SIZE,
+      skip,
+    }),
+    prisma.post.count({
+      where: {
+        published: true,
+      },
+    }),
+  ]);
+
+  const pageCount = Math.ceil(count / PAGE_SIZE);
+
   return {
-    props: { feed },
+    props: { feed, pageCount, page },
   };
 };
 
 type Props = {
   feed: PostProps[];
+  pageCount: number;
+  page: number;
 };
 
-const Blog: React.FC<Props> = (props) => {
+const Blog: React.FC<Props> = ({ feed, pageCount, page }) => {
+  const pages = [];
+
+  const handlePageChange = (newPage: number) => {
+    // Prevent page change if already on the same page
+    if (newPage === page) return;
+
+    // Navigate to the new page
+    window.location.href = `/?page=${newPage}`;
+  };
+
+  for (let i = 1; i <= pageCount; i++) {
+    pages.push(
+      <button
+        key={i}
+        className={i === page ? "active" : ""}
+        onClick={() => handlePageChange(i)}
+        disabled={i === page}
+        aria-disabled={i === page}
+      >
+        {i}
+      </button>
+    );
+  }
+
   return (
     <Layout>
       <div className="page">
         <h1>Public Feed</h1>
         <main>
-          {props.feed.map((post) => (
+          {feed.map((post) => (
             <div key={post.id} className="post">
               <Post post={post} />
             </div>
           ))}
         </main>
+        <div className="pagination">
+          <button
+            className={page === 1 ? "disabled" : ""}
+            disabled={page === 1}
+            onClick={() => handlePageChange(page - 1)}
+          >
+            &laquo;
+          </button>
+          {pages}
+          <button
+            className={page === pageCount ? "disabled" : ""}
+            disabled={page === pageCount}
+            onClick={() => handlePageChange(page + 1)}
+          >
+            &raquo;
+          </button>
+        </div>
       </div>
       <style jsx>{`
         .post {
@@ -52,9 +112,36 @@ const Blog: React.FC<Props> = (props) => {
         .post + .post {
           margin-top: 2rem;
         }
+
+        .pagination {
+          display: flex;
+          justify-content: center;
+          margin-top: 2rem;
+        }
+
+        .pagination button {
+          margin: 0 0.5rem;
+          font-size: 1.2rem;
+          border: none;
+          background: none;
+          cursor: pointer;
+        }
+
+        .pagination .active {
+          font-weight: bold;
+          color: gray;
+          pointer-events: none;
+        }
+
+        .pagination .disabled {
+          color: #aaa;
+          pointer-events: none;
+        }
       `}</style>
     </Layout>
   );
 };
+
+
 
 export default Blog;
