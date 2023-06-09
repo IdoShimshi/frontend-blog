391a7aa35fff978080b93592d02f084f7f9e73d1
unknown <OrenHaviv84@gmail.com>
2023-06-09 18:58:13 +0300

    initialize token getter and setter, and sending them as props to all components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import { SessionProvider } from "next-auth/react";
 import { AppProps } from "next/app";
+import { useState } from "react";
 
 const App = ({ Component, pageProps }: AppProps) => {
+  const [token, setToken] = useState(null)
   return (
     <SessionProvider session={pageProps.session}>
-      <Component {...pageProps} />
+      <Component {...{...pageProps, token: token, setToken: setToken}} />
     </SessionProvider>
   );
 };
